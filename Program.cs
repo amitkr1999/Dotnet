bd7757a097d01bba19882255866938de015dc69d
Amit <amitkr21999@gmail.com>
2023-08-26 19:33:48 +0530

    Variable, Array,  Operators

--- Program.cs
+++ Program.cs
@@ -20,53 +20,65 @@ namespace HelloWorld{
             //// 00001000 = 8
  
            // 1 byte (8 bit) unsigned, where signed means it can be negative
-            byte myByte = 255;
-            byte mySecondByte = 0;
+            // byte myByte = 255;
+            // byte mySecondByte = 0;
  
-            // 1 byte (8 bit) signed, where signed means it can be negative
-            sbyte mySbyte = 127;
-            sbyte mySecondSbyte = -128;
- 
- 
-            // 2 byte (16 bit) unsigned, where signed means it can be negative
-            ushort myUshort = 65535;
- 
-            // 2 byte (16 bit) signed, where signed means it can be negative
-            short myShort = -32768;
- 
-            // 4 byte (32 bit) signed, where signed means it can be negative
-            int myInt = 2147483647;
-            int mySecondInt = -2147483648;
- 
-            // 8 byte (64 bit) signed, where signed means it can be negative
-            long myLong = -9223372036854775808;
+            // // 1 byte (8 bit) signed, where signed means it can be negative
+            // sbyte mySbyte = 127;
+            // sbyte mySecondSbyte = -128;
  
  
-            // 4 byte (32 bit) floating point number
-            float myFloat = 0.751f;
-            float mySecondFloat = 0.75f;
+            // // 2 byte (16 bit) unsigned, where signed means it can be negative
+            // ushort myUshort = 65535;
  
-            // 8 byte (64 bit) floating point number
-            double myDouble = 0.751;
-            double mySecondDouble = 0.75d;
+            // // 2 byte (16 bit) signed, where signed means it can be negative
+            // short myShort = -32768;
  
-            // 16 byte (128 bit) floating point number
-            decimal myDecimal = 0.751m;
-            decimal mySecondDecimal = 0.75m;
+            // // 4 byte (32 bit) signed, where signed means it can be negative
+            // int myInt = 2147483647;
+            // int mySecondInt = -2147483648;
  
-            // Console.WriteLine(myFloat - mySecondFloat);
-            // Console.WriteLine(myDouble - mySecondDouble);
-            // Console.WriteLine(myDecimal - mySecondDecimal);
+            // // 8 byte (64 bit) signed, where signed means it can be negative
+            // long myLong = -9223372036854775808;
  
  
+            // // 4 byte (32 bit) floating point number
+            // float myFloat = 0.751f;
+            // float mySecondFloat = 0.75f;
  
-            string myString = "Hello World";
-            // Console.WriteLine(myString);
-            string myStringWithSymbols = "!@#$@^$%%^&(&%^*__)+%^@##$!@%123589071340698ughedfaoig137";
-            // Console.WriteLine(myStringWithSymbols);
+            // // 8 byte (64 bit) floating point number
+            // double myDouble = 0.751;
+            // double mySecondDouble = 0.75d;
  
-            bool myBool = true;
+            // // 16 byte (128 bit) floating point number
+            // decimal myDecimal = 0.751m;
+            // decimal mySecondDecimal = 0.75m;
+ 
+            // // Console.WriteLine(myFloat - mySecondFloat);
+            // // Console.WriteLine(myDouble - mySecondDouble);
+            // // Console.WriteLine(myDecimal - mySecondDecimal);
+ 
+ 
+ 
+            // string myString = "Hello World";
+            // // Console.WriteLine(myString);
+            // string myStringWithSymbols = "!@#$@^$%%^&(&%^*__)+%^@##$!@%123589071340698ughedfaoig137";
+            // // Console.WriteLine(myStringWithSymbols);
+ 
+            // bool myBool = true;
+
+            String[] myGroceryArray = new string[2];
+
+            myGroceryArray[0]   = "Apple";
+            myGroceryArray[1]   = "Ice Cream";
+
+            Console.WriteLine(myGroceryArray[0]);
+            Console.WriteLine(myGroceryArray[1]);
+
+            string[] mysecondGroceryArray = {"Mango", "Banana",};
             
+            Console.WriteLine(mysecondGroceryArray[0]);
+            Console.WriteLine(mysecondGroceryArray[1]);
         }
     }
 }
